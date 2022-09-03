28f0d1a78d2a72e5ca91869d81e68f06675f52c0
RybalovMS <RybalovMS@313D-5>
2022-09-03 10:30:10 +0700

    Пв

--- ProjectNSU1/Program.cs
+++ ProjectNSU1/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-namespace ProjectNSU1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+int x, y;
+Console.WriteLine("Введите значение для X >");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение для Y >");
+y = Convert.ToInt32(Console.ReadLine());
+//int c = x;
+//x = y;
+//y = c;
+x = x - y;
+y = x + y;
+x = y - x; 
+Console.WriteLine($"x = {x} y =  {y}");
